c58b7ddb96c944d63eae3b97fd2b474d1e1ef8d3
BHammock33 <105878596+BHammock33@users.noreply.github.com>
2023-06-27 15:34:30 -0400

    eclipse commit

--- src/main/java/com/coderscampus/reactbackend/repository/TodoRepository.java
+++ src/main/java/com/coderscampus/reactbackend/repository/TodoRepository.java
@@ -1,9 +1,12 @@
 package com.coderscampus.reactbackend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.coderscampus.reactbackend.domain.TodoItem;
 
 public interface TodoRepository extends JpaRepository<TodoItem, Long>{
 
+	List<TodoItem> findByIsDone(Boolean isDone);
 }
--- src/main/java/com/coderscampus/reactbackend/web/TodoController.java
+++ src/main/java/com/coderscampus/reactbackend/web/TodoController.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coderscampus.reactbackend.domain.TodoItem;
@@ -32,10 +33,35 @@ public class TodoController {
 
 		return ResponseEntity.ok(allTodoItems);
 	}
-	@PostMapping
+	@GetMapping("/active")
+	public ResponseEntity<List<TodoItem>> getActiveTodoItems(){
+		List<TodoItem> allActiveTodoItems = null;
+		
+		try {
+			allActiveTodoItems = todoRepo.findByIsDone(false);
+		}catch (Exception e) {
+			return ResponseEntity.status(500).build();
+		}
+
+		return ResponseEntity.ok(allActiveTodoItems);
+	}
+	
+	
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
 	public ResponseEntity<List<TodoItem>> addNewTodoItem(@RequestBody TodoItem todoItem){
 		todoRepo.save(todoItem);
 		
+		return getAllTodoItems();
+	}
+//	@PutMapping
+//	public ResponseEntity<?> updateTodoItem(@RequestBody TodoItem todoItem){
+//		todoRepo.save(todoItem);
+//		
+//		return getAllTodoItems();
+//	}
+	@DeleteMapping
+	public ResponseEntity<List<TodoItem>> deleteTodoItem(@RequestBody TodoItem todoItem){
+		todoRepo.deleteById(todoItem.getId());
 		return getAllTodoItems();
 	}
 }
